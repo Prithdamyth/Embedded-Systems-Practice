b162e2993380fc5d95369b415f22fdbfb7389210
Prithvi Velpuri <pvelpur@purdue.edu>
2019-01-09 17:20:29 -0500

    HW2

---
+++ HW2/Debug/src/subdir.mk
@@ -0,0 +1,46 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/stm32f0xx_it.c \
+../src/syscalls.c \
+../src/system_stm32f0xx.c 
+
+S_UPPER_SRCS += \
+../src/main.S 
+
+OBJS += \
+./src/main.o \
+./src/stm32f0xx_it.o \
+./src/syscalls.o \
+./src/system_stm32f0xx.o 
+
+S_UPPER_DEPS += \
+./src/main.d 
+
+C_DEPS += \
+./src/stm32f0xx_it.d \
+./src/syscalls.d \
+./src/system_stm32f0xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.S
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m0 -mthumb -mfloat-abi=soft -DSTM32 -DSTM32F0 -DSTM32F051R8Tx -DSTM32F0DISCOVERY -DDEBUG -DSTM32F051x8 -DUSE_HAL_DRIVER -I"/home/prith123/workspace/HW2/HAL_Driver/Inc/Legacy" -I"/home/prith123/workspace/HW2/Utilities/STM32F0308-Discovery" -I"/home/prith123/workspace/HW2/inc" -I"/home/prith123/workspace/HW2/CMSIS/device" -I"/home/prith123/workspace/HW2/CMSIS/core" -I"/home/prith123/workspace/HW2/HAL_Driver/Inc" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m0 -mthumb -mfloat-abi=soft -DSTM32 -DSTM32F0 -DSTM32F051R8Tx -DSTM32F0DISCOVERY -DDEBUG -DSTM32F051x8 -DUSE_HAL_DRIVER -I"/home/prith123/workspace/HW2/HAL_Driver/Inc/Legacy" -I"/home/prith123/workspace/HW2/Utilities/STM32F0308-Discovery" -I"/home/prith123/workspace/HW2/inc" -I"/home/prith123/workspace/HW2/CMSIS/device" -I"/home/prith123/workspace/HW2/CMSIS/core" -I"/home/prith123/workspace/HW2/HAL_Driver/Inc" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
